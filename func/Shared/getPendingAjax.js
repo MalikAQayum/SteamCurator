da27ace2c767d2e24037881612afae29b9501784
MalikAQayum <l_malik_l@hotmail.com>
2019-03-13 17:12:02 +0100

    Update getPendingAjax.js

--- func/Shared/getPendingAjax.js
+++ func/Shared/getPendingAjax.js
@@ -1,4 +1,6 @@
 function getPendingAjax(clanid){
+        var clanid="33779114-pcgameit";
+
     GM_xmlhttpRequest({
         method: "GET",
         url: "https://store.steampowered.com/curator/"+clanid+"/admin/pending?ajax=1",
