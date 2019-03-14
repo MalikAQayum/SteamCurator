61ad75c286b25f2e489f1ebe0a9e43110ea3f19f
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 17:00:47 +0100

    localStorage.getItem > GM.getValue
    moving to GM.getValue

--- func/Store/validateStorage_2.js
+++ func/Store/validateStorage_2.js
@@ -1,7 +1,19 @@
+/* localStorage.getItem
 function validateStorage_2(clanid){
     if ((localStorage.getItem("store_pAppids") !== null) &&
         (localStorage.getItem("store_eAppids") !== null)){
         var sourcePages = localStorage.getItem("store_pAppids").concat(","+localStorage.getItem("store_eAppids"));
+        handleStoreData(clanid,sourcePages);
+    }else{
+        setTimeout(function(){validateStorage_2(clanid);}, 150);
+    }
+}
+*/
+//GM.getValue
+function validateStorage_2(clanid){
+    if ((GM.getValue("store_pAppids") !== null) &&
+        (GM.getValue("store_eAppids") !== null)){
+        var sourcePages = GM.getValue("store_pAppids").concat(","+GM.getValue("store_eAppids"));
         handleStoreData(clanid,sourcePages);
     }else{
         setTimeout(function(){validateStorage_2(clanid);}, 150);
