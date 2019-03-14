8bdf2387b2b45bf32c452f6eb01a624899390507
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 18:30:31 +0100

    Update SteamCurator.user.js

--- SteamCurator.user.js
+++ SteamCurator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Curator
 // @namespace    https://malikaqayum.github.io/SteamCurator/
-// @version      0.24
+// @version      0.25
 // @description  Does Curator Stuff.
 // @author       MalikQayum
 // @connect      api.steampowered.com
