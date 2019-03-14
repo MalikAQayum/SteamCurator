9cddef2221c8a42abfb86c08f9c08a2eb12abd6e
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 08:46:53 +0100

    Update ClearlocalStorage.js

--- func/Curator/ClearlocalStorage.js
+++ func/Curator/ClearlocalStorage.js
@@ -7,7 +7,7 @@ function ClearlocalStorage_0(){
     localStorage.removeItem('eAppids');
 }
 function ClearlocalStorage_1(){
-    localStorage.clear();
+    //localStorage.clear();
     localStorage.removeItem('aAppids');
     localStorage.removeItem('r3o_old');
 }
