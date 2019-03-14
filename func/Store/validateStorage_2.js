d57279ce71ed77a2df9d5d815a7ca3bb4922e5d4
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 08:58:59 +0100

    Update validateStorage_2.js

--- func/Store/validateStorage_2.js
+++ func/Store/validateStorage_2.js
@@ -1,9 +1,10 @@
-function validateStorage_2(clanid){
+function validateStorage_2(){
+    var clanid=localStorage.getItem("myclanid");
     if ((localStorage.getItem("store_pAppids") !== null) &&
         (localStorage.getItem("store_eAppids") !== null)){
         var sourcePages = localStorage.getItem("store_pAppids").concat(","+localStorage.getItem("store_eAppids"));
-        handleStoreData(clanid,sourcePages);
+        handleStoreData(sourcePages);
     }else{
-        setTimeout(function(){validateStorage_2(clanid);}, 150);
+        setTimeout(function(){validateStorage_2();}, 150);
     }
 }
