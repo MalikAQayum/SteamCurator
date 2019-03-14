c04e74025d819cc3bed03b3174ad463a1cb79bb1
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 17:20:02 +0100

    localStorage > GM

--- func/Curator/validateStorage.js
+++ func/Curator/validateStorage.js
@@ -1,3 +1,4 @@
+/* localStorage
 function validateStorage_0(){
     if ((localStorage.getItem("eSource") !== null) &&
         (localStorage.getItem("pSource") !== null) &&
@@ -15,6 +16,30 @@ function validateStorage_1(){
         (localStorage.getItem("pSource") !== null) &&
         (localStorage.getItem("aAppids") !== null)){
         var sourcePages = localStorage.getItem("pSource").concat(localStorage.getItem("eSource"));
+        handleData(sourcePages);
+    }else{
+        setTimeout(function(){validateStorage_1();}, 150);
+    }
+}
+*/
+//GM
+function validateStorage_0(){
+    if ((GM.getValue("eSource") !== null) &&
+        (GM.getValue("pSource") !== null) &&
+        (GM.getValue("pAppids") !== null) &&
+        (GM.getValue("eAppids") !== null)){
+        var sourcePages = GM.getValue("pSource").concat(GM.getValue("eSource"));
+        handleData(sourcePages);
+    }else{
+        setTimeout(function(){validateStorage_0();}, 150);
+    }
+}
+
+function validateStorage_1(){
+    if ((GM.getValue("eSource") !== null) &&
+        (GM.getValue("pSource") !== null) &&
+        (GM.getValue("aAppids") !== null)){
+        var sourcePages = GM.getValue("pSource").concat(GM.getValue("eSource"));
         handleData(sourcePages);
     }else{
         setTimeout(function(){validateStorage_1();}, 150);
