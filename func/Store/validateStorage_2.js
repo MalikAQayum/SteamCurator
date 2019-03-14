a1ac34cad541222c89693a306f0f3347c157630d
MalikAQayum <l_malik_l@hotmail.com>
2019-03-14 09:48:48 +0100

    Update validateStorage_2.js

--- func/Store/validateStorage_2.js
+++ func/Store/validateStorage_2.js
@@ -2,7 +2,6 @@ function validateStorage_2(clanid){
     if ((localStorage.getItem("store_pAppids") !== null) &&
         (localStorage.getItem("store_eAppids") !== null)){
         var sourcePages = localStorage.getItem("store_pAppids").concat(","+localStorage.getItem("store_eAppids"));
-        console.log("validateStorage_2 done");
         handleStoreData(clanid,sourcePages);
     }else{
         setTimeout(function(){console.log("sourcePages not set yet"); validateStorage_2(clanid);}, 150);
