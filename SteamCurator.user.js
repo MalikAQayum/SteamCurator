f4c588f7067ce3116ea919bb82afbc332e19f050
Malik Qayum <malik.qayum@hotmail.com>
2019-03-23 02:25:10 +0100

    better add through curator button

--- SteamCurator.user.js
+++ SteamCurator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Curator
 // @namespace    https://malikaqayum.github.io/SteamCurator/
-// @version      0.20
+// @version      0.21
 // @description  Does Curator Stuff.
 // @author       MalikQayum
 // @connect      api.steampowered.com
--- func/Store/handleStoreData.js
+++ func/Store/handleStoreData.js
@@ -3,10 +3,18 @@ function handleStoreData(clanid,sourcePages){
     var peAppid = sourcePages.includes($J(location).attr("href").split("/")[4]);
     console.log(peAppid);
     if (peAppid === true){
-        if ($J(".btn_addtocart")[0]){
-            $J( ".btn_addtocart:first" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending"><span>Add through Curator</span></a>' );
-        } else {
-            $J( "#game_area_purchase > div.game_area_comingsoon.game_area_bubble > div > p" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending" style="float: right;"><span>Add through Curator</span></a>' );
+        if ($J(".game_area_purchase_game.demo_above_purchase")[0]){
+            $J( ".btn_addtocart:eq(1)" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending"><span>Add through Curator</span></a>' );
+        }else{
+            if ($J(".btn_addtocart")[0]){
+                $J( ".btn_addtocart:first" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending"><span>Add through Curator</span></a>' );
+            } else {
+                if ($J( "#game_area_purchase > div.game_area_comingsoon.game_area_bubble > div > p" )[0]){
+                    $J( "#game_area_purchase > div.game_area_comingsoon.game_area_bubble > div > p" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending" style="float: right;"><span>Add through Curator</span></a>' );
+                }else{
+                    $J( "#game_area_purchase > div.game_area_comingsoon.game_area_bubble > div" ).append( '<a class="btnv6_blue_hoverfade btn_medium" href="https://store.steampowered.com/curator/'+clanid+'/admin/pending" style="float: right;"><span>Add through Curator</span></a>' );
+                }
+            }
         }
     }
 }
